49d54155533ef7705c299f4557f6cad81cc3b1fd
SergioBJA <sergiobohorquezj@gmail.com>
2026-05-05 17:25:02 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-window.ACADEMY_CONFIG = {
-    API_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
-             ? 'http://localhost:8000/api' 
-             : 'https://TU-URL-DE-VERCEL.vercel.app/api'
-};
